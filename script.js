d415b686f857732d1fb8a30010184f0847b97c70
Hetoreno <hectortamriel@gmail.com>
2020-07-22 18:19:58 -0700

    updating

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const value = document.querySelector(".value");
 const submit = document.querySelector(".submit");
+const clear = document.querySelector(".clear");
 const list = document.querySelector(".list");
-
+const all = document.querySelector(".ul-cont");
 
 submit.addEventListener("click",addingItems);
-submit.addEventListener("click",addingItems);
-
+list.addEventListener("click",deleteOne);
+clear.addEventListener("click",deleteAll);
+//making entire list
 function addingItems(event){
     const itemCont = document.createElement("div");
     itemCont.classList.add("todo");
+    list.appendChild(itemCont);
 
     const newItem = document.createElement("li");
     newItem.classList.add('item');
@@ -21,14 +24,26 @@ function addingItems(event){
     deleteButton.innerText = "Delete";
     itemCont.appendChild(deleteButton);
 
-    list.appendChild(itemCont);
-}
-  
-function deleteOne(){
-
+    
 }
 
+//abilty to delete one item from list
+function deleteOne(event){
+    const clickItem= event.target;
+    console.log(clickItem);
+    
+    if (clickItem.classList[0] === 'dele-button'){
+        const wholeList = clickItem.parentElement;
+        wholeList.remove();
+    }
+}
+
+//abilty to delete entire list
 function deleteAll(e){
 
+    while (all.children.length>0){
+        all.removeChild(all.children[0]);
+        clearStorage();
+    }
 }
 
